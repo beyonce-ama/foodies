80b9a1de8420e7d2a2df5f1c47dd9359619d7294
beyonce-ama <amabey222@gmail.com>
2025-04-02 18:20:43 +0800

    added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,15 +27,24 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = System.getenv("CM_BUILD_ID")?.toInt()
+        versionName = "1.0.${System.getenv("CM_BUILD_ID")?.toInt()}"
+    }
+
+ signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("CM_KEYSTORE_PATH")) 
+            storePassword = System.getenv("CM_KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("CM_KEY_ALIAS")
+            keyPassword = System.getenv("CM_KEY_PASSWORD")
+        }
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
